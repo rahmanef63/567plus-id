61eb4c575d59b30dca0c87540bebd065eac3a935
rahmanef63 <rahmanef63@gmail.com>
2024-08-21 01:55:16 +0700

    test webhook

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <h1 className="mb-5 text-5xl font-bold">laundry 567 plus</h1>
                     <p className="mb-5">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
