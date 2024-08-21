4c5240c338b883a3a5d2366973607ade70674eb6
rahmanef63 <rahmanef63@gmail.com>
2024-08-21 20:13:07 +0700

    update nextconfig.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         loader: 'custom',
         loaderFile: './my-loader.ts',
     },
+    output: 'export',
     trailingSlash: true,
 };
 
