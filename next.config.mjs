47d87ffeba3a7c9c235ee5f534773650575f4de6
rahmanef63 <rahmanef63@gmail.com>
2024-08-21 20:48:38 +0700

    update nectconfigmjs onemore time because the svg is not loaded

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
     },
     output: 'export',
     trailingSlash: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
