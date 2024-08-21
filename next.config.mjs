b340be883657e6f32f627c55b53d55fca392fc39
rahmanef63 <rahmanef63@gmail.com>
2024-08-21 21:31:46 +0700

    updating nextconfig removing export out

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ const nextConfig = {
         loader: 'custom',
         loaderFile: './my-loader.ts',
     },
-    output: 'export',
-    trailingSlash: true,
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
-        return config;
-    },
+    // output: 'export',
+    // trailingSlash: true,
+    // webpack(config) {
+    //     config.module.rules.push({
+    //         test: /\.svg$/,
+    //         use: ['@svgr/webpack'],
+    //     });
+    //     return config;
+    // },
 };
 
 export default nextConfig;
